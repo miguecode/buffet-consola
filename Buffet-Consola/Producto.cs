4bb39fba0a76de7a074d972656341957be0acd76
Leumig <junmigue7@gmail.com>
2024-09-09 18:22:30 -0300

    correcciones

---
+++ Buffet-Consola/Producto.cs
@@ -0,0 +1,30 @@
+﻿namespace Buffet_Consola
+{
+    public class Producto
+    {
+        private string nombre;
+        private int precio;
+        private int cantidad;
+
+        public Producto(string nombre, int precio, int cantidad)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.cantidad = cantidad;
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public int Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+    }
+
+}
